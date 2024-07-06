c692464b4d94541e6ab764ec23f8a740090481b6
kingmafiaa <hajisahb724@gmail.com>
2024-07-06 17:30:59 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '+994 40 539 60 94' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=ndtQQLqJ#B6-smGDlfYnylUpbnAQyudKN0KWwmnKyIdm-j_ZmZGg' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zYchxbQY#h9nD78B_RZvHvLQeG1Ny3IRoNl2gN4Ohr-ApBYVX2-4' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
